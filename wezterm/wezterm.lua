51b5a0451c424f961e2b0828956fd6f3da0410c0
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-05-02 09:25:57 -0500

    feat: remove padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -62,7 +62,7 @@ return {
 	adjust_window_size_when_changing_font_size = false,
 	window_decorations = "INTEGRATED_BUTTONS",
 	window_padding = {
-		left = 5,
+		left = 0,
 		right = 0,
 		top = 0,
 		bottom = 0,
